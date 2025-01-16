ff4feb5a60320a55663b3cba0650a9cb8b30738d
mbzxn <104250450+mbzxn@users.noreply.github.com>
2025-01-16 15:22:36 +0800

    Add files via upload

---
+++ script/Step1_data_split.sh
@@ -0,0 +1,7 @@
+cd D:\project\TransCDR-master
+python Step1_Data_split.py \
+--model_type "regression" \
+--scenarios "warm start" \
+--n_clusters 0 \
+--n_sampling 0 \
+--result_folder "./data/GDSC"
